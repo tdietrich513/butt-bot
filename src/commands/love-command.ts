7517acb887401cced50289cf2a5e00b8cc6f55d3
Tom Dietrich <tom.dietrich@gmail.com>
2020-08-11 20:14:44 -0400

    custom react for friends

--- src/commands/love-command.ts
+++ src/commands/love-command.ts
@@ -12,8 +12,12 @@ class LoveCommand extends Command {
     exec(message: Message, args: any) : any {
         if (Math.random() > 0.25) return;
         
-        message.react("❤️")
-            .catch(() => console.error("Failed to react"));
+        if (message.guild.id == "688518824433025035")
+            message.react("742895857568972861")
+                .catch(() => console.error("Failed to react"));
+        else
+            message.react("❤️")
+                .catch(() => console.error("Failed to react"));
     }
 }
 
